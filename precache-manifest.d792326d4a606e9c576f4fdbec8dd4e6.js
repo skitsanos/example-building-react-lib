ceb518ff0cd1d6f0f6749de8d3e083272aa6d25c
skitsanos <info@skitsanos.com>
2022-05-11 08:50:22 +0300

    Updates

---
+++ precache-manifest.d792326d4a606e9c576f4fdbec8dd4e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e80c6378b138d88b3d07d0347c3eea79",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d84a942a67e716d166a2",
+    "url": "./static/css/main.b693b373.chunk.css"
+  },
+  {
+    "revision": "a5811a1cdc3506cd99fe",
+    "url": "./static/js/2.190cfb81.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.190cfb81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d84a942a67e716d166a2",
+    "url": "./static/js/main.513aeac8.chunk.js"
+  },
+  {
+    "revision": "332fc18e26dccd5d023a",
+    "url": "./static/js/runtime-main.aa85c257.js"
+  }
+]);
